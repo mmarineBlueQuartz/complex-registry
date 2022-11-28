71af879792c0544a97e98d1fd9519b855b10d49d
Jared Duffey <jared.duffey@bluequartz.net>
2022-11-28 17:24:55 -0500

    [boost-mp11] Replaced FetchContent to fix issue with latest vcpkg
    * See https://github.com/microsoft/vcpkg/pull/26959
    
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

--- ports/boost-mp11/portfile.cmake
+++ ports/boost-mp11/portfile.cmake
@@ -8,6 +8,17 @@ vcpkg_from_github(
     REF boost-${MP11_VERSION}
     SHA512 02a93db3c0ee65b6742109c369c70300465ebc7d824f4c9bbf8f083395e68f0a4d2b4def8299f2fc29bc5c7cb0b18dc5a4d96d1f0e20216a49986ce9387ca5d1
     HEAD_REF master
+    PATCHES
+        disable_fetchcontent.patch
+)
+
+# We manually clone boostorg/cmake and set boostorg_cmake_SOURCE_DIR because vcpkg now sets FETCHCONTENT_FULLY_DISCONNECTED. See https://github.com/microsoft/vcpkg/pull/26959
+vcpkg_from_github(
+    OUT_SOURCE_PATH BOOST_CMAKE_SOURCE_PATH
+    REPO boostorg/cmake
+    REF boost-${MP11_VERSION}
+    SHA512 1b684655d9ffc8b48eb509248e933399c27e95f6f2657cdeb9542c5b3e56c9d2c377fa92e82d51d516cb37e2df77a002d5979886adfd627210c02337dff71216
+    HEAD_REF master
 )
 
 vcpkg_find_acquire_program(GIT)
@@ -18,6 +29,7 @@ vcpkg_configure_cmake(
     OPTIONS
         -DBUILD_TESTING=OFF
         -DGIT_EXECUTABLE=${GIT}
+        -Dboostorg_cmake_SOURCE_DIR=${BOOST_CMAKE_SOURCE_PATH}
 )
 
 vcpkg_install_cmake()
